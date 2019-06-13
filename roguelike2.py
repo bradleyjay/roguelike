08f7fcf007ed25d57f803ad2045ea93cc833036e
bradleyjay <bradleyjshields@gmail.com>
2019-06-13 16:41:46 -0500

    Rewrite - add NPC, un-factor.

--- roguelike2.py
+++ roguelike2.py
@@ -12,7 +12,36 @@ LIMIT_FPS = 20  # 20 frames-per-second maximum
 # Game Controls
 TURN_BASED = True  # turn-based game
  
- 
+##################
+## KEY CLASSES ###
+##################
+
+class Object:
+    # catch-all object class. Player, monsters, item, everything will be a character on-screen.
+
+    def __init__(self, x, y, char, color):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    def move(self,dx,dy):
+        # move by a delta
+        self.x += dx
+        self.y += dy
+
+    def draw(self):
+        # set color, draw char at this position
+        tcod.console_set_default_foreground(con, self.color)
+        tcod.console_put_char(con, self.x, self.y, self.char, tcod.BKGND_NONE)
+
+    def clear(self):
+        # erase this character that represents this obj
+        tcod.console_put_char(con, self.x, self.y, ' ', tcod.BKGND_NONE)
+
+
+
+
 # ######################################################################
 # User Input
 # ######################################################################
@@ -27,7 +56,6 @@ def get_key_event(turn_based=None):
  
  
 def handle_keys():
-    global player_x, player_y
  
     key = get_key_event(TURN_BASED)
  
@@ -40,16 +68,16 @@ def handle_keys():
  
     # movement keys
     if tcod.console_is_key_pressed(tcod.KEY_UP):
-        player_y -= 1
+        player.move(0,-1)
  
     elif tcod.console_is_key_pressed(tcod.KEY_DOWN):
-        player_y += 1
+        player.move(0,1)
  
     elif tcod.console_is_key_pressed(tcod.KEY_LEFT):
-        player_x -= 1
+        player.move(-1,0)
  
     elif tcod.console_is_key_pressed(tcod.KEY_RIGHT):
-        player_x += 1
+        player.move(0,1)
  
  
 #############################################
@@ -57,33 +85,44 @@ def handle_keys():
 #############################################
  
  
-def main():
-    # Setup player
-    global player_x, player_y
-    player_x = SCREEN_WIDTH // 2
-    player_y = SCREEN_HEIGHT // 2
+
+player = Object(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, '@', tcod.white)
+npc = Object(SCREEN_WIDTH // 2 - 5, SCREEN_HEIGHT // 2, '@', tcod.yellow)
+
+objects = [npc, player]
+
+# Setup Font
+font_filename = 'arial10x10.png'
+tcod.console_set_custom_font(font_filename, tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
+
+# Initialize screen, then buffer console
+title = 'Python 3 + Libtcod tutorial'
+tcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, title, FULLSCREEN)
+
+# buffer console
+con = tcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+# Set FPS
+tcod.sys_set_fps(LIMIT_FPS)
+
+exit_game = False
+while not tcod.console_is_window_closed() and not exit_game:
+
+        #draw all objects in the list
+    for object in objects:
+        object.draw()
+
+    #blit the contents of "con" to the root console and present it
+    tcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+    tcod.console_flush()
+
+    #erase all objects at their old locations, before they move
+    for object in objects:
+        object.clear()
+
+    #handle keys and exit game if needed
+    exit = handle_keys()
+    if exit:
+        break
+
  
-    # Setup Font
-    font_filename = 'arial10x10.png'
-    tcod.console_set_custom_font(font_filename, tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
- 
-    # Initialize screen
-    title = 'Python 3 + Libtcod tutorial'
-    tcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, title, FULLSCREEN)
-    
-    #initialize new console for other drawing
-    con = tcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
-    # Set FPS
-    tcod.sys_set_fps(LIMIT_FPS)
- 
-    exit_game = False
-    while not tcod.console_is_window_closed() and not exit_game:
-        tcod.console_set_default_foreground(0, tcod.white)
-        tcod.console_put_char(0, player_x, player_y, '@', tcod.BKGND_NONE)
-        tcod.console_flush()
-        tcod.console_put_char(0, player_x, player_y, ' ', tcod.BKGND_NONE)
- 
-        exit_game = handle_keys()
- 
- 
-main()
