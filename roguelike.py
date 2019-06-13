3e3ec433f016ddd48cce7058eceb6e5d3ae3c561
bradleyjay <bradleyjshields@gmail.com>
2019-06-13 15:18:07 -0400

    Added utility commands and movement keypresses listening.

--- roguelike.py
+++ roguelike.py
@@ -1,6 +1,7 @@
 # import libcodpy as tcod # looks deprecated
 import tcod
 
+# static variables
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
@@ -17,11 +18,47 @@ tcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, window_title, fullscreen)
 # Want turnbased? Comment this line
 tcod.sys_set_fps(LIMIT_FPS)
 
+# player init variables
+player_x = SCREEN_WIDTH // 2
+player_y = SCREEN_HEIGHT // 2
+
+# game functions
+
+def handle_keys():
+    
+    # check for utility keys - fullscreen, exit
+    key = tcod.console_check_for_keypress()
+
+    if key.vk == tcod.KEY_ENTER and key.lalt:
+        #Alt+Enter: toggle fullscreen
+        tcod.console_set_fullscreen(not tcod.console_is_fullscreen())
+ 
+    elif key.vk == tcod.KEY_ESCAPE:
+        return True  #exit game
+ 
+    # movement keys
+    global player_x, player_y
+    
+    if tcod.console_is_key_pressed(tcod.KEY_UP):
+        player_y = player_y - 1
+ 
+    elif tcod.console_is_key_pressed(tcod.KEY_DOWN):
+        player_y = player_y + 1
+ 
+    elif tcod.console_is_key_pressed(tcod.KEY_LEFT):
+        player_x = player_x - 1
+ 
+    elif tcod.console_is_key_pressed(tcod.KEY_RIGHT):
+        player_x = player_x + 1
+
+
+
 
 # main loop
 while not tcod.console_is_window_closed():
 
     # '0' is which console we're printing to - here, that's the screen
+    # note: drawing before handling key input
     tcod.console_set_default_foreground(0, tcod.white)
 
     # print character
@@ -29,3 +66,10 @@ while not tcod.console_is_window_closed():
 
     # flush changes to screen
     tcod.console_flush()
+
+    # clean space where player just was by printing a space there
+    tcod.console_put_char(0, player_x, player_y, ' ', tcod.BKGND_NONE)
+     #handle keys and exit game if needed
+    exit = handle_keys()
+    if exit:
+        break
